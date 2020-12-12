322958d6f39bd26012dde36befe60e4d977a5965
Yipkei Kwok <ykwok@missouriwestern.edu>
2020-12-12 12:21:58 -0600

    20201212at1221

--- cache_simulator/cache_simulator.cpp
+++ cache_simulator/cache_simulator.cpp
@@ -1,13 +1,34 @@
 #include <fstream> 
 #include <iostream> 
+#include <unordered_map> 
+#include <utility> // std::pair
+#include <boost/functional/hash.hpp> // boost::hash<>
 
 int main(void) {
     uint64_t id; 
     uint64_t size; 
     uint8_t decision;
+    // uint64_t timestamp;
+    std::unordered_map<
+        std::pair<uint64_t, uint64_t>, //<id, size> 
+        uint8_t, // decision
+        boost::hash<std::pair<uint64_t, uint64_t>>
+        > cacheMap; 
 
+    uint64_t timestamp=(uint64_t)0; 
     while(std::cin >> id >> size >> decision) {
-        std::cout<<id<<" "<<size<<" "<<decision<<std::endl;
+        const std::pair<uint64_t, uint64_t> id_size = std::make_pair(id, size); 
+        cacheMap[id_size] = decision; 
+        timestamp++; 
     }
+    for(const auto& it: cacheMap) {
+        // std::cout<<it.first<<" "<<it.second<<std::endl;
+        const auto& pair0 = it.first;
+        std::cout<<pair0.first<<" "<<pair0.second<<" "<<it.second<<std::endl;
+        //uint64_t uint0 = it.second;
+        //std::cout<<uint0<<std::endl;
+        //std::cout<<cacheMap[it.first]<<std::endl;
+    }
+
     return 0;
 }
